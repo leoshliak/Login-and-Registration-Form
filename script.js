22685de9ec186c42e55ed07c4c7a9c26099d0d58
leoshliak <139138564+leoshliak@users.noreply.github.com>
2024-04-22 19:53:14 +0300

    passwords validation

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ signInLink.addEventListener('click', () => {
     wrapper.classList.toggle('active');
   });
 
+ function checkPasswords(){
+    const password = document.getElementById('password2');
+    const confirmPassword = document.getElementById('confirm-password');
+
+    if(password.value !== confirmPassword.value || confirmPassword.value == ''){
+     confirmPassword.setCustomValidity('Passwords Must Match');
+        confirmPassword.reportValidity();
+
+        confirmPassword.style.color = '#ffffff43';
+    }else{
+        confirmPassword.setCustomValidity('');
+        confirmPassword.style.color = '#fff';
+    }
+}
