04da7641a36f82dd2494c114f82a89c7622e2fd8
leoshliak <139138564+leoshliak@users.noreply.github.com>
2024-04-22 19:52:26 +0300

    switch forms

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const signInLink = document.querySelector('.sign__in-link');
+const signUpLink = document.querySelector('.sign__up-link');
+const wrapper = document.querySelector('.wrapper');
+
+signUpLink.addEventListener('click', () => {
+  wrapper.classList.toggle('active');
+});
+
+signInLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+  });
 
